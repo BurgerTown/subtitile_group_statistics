06c95ad026307a1e3a374a34422dd1d5279e2fa5
BurgerTown <BurgerTownLuo@gmail.com>
2019-07-05 11:28:47 +0800

    修复example.xlsx一个小错误 example.xlsx -> Example.xlsx 增加重复检测功能 增加命令行输出

--- main.py
+++ main.py
@@ -4,25 +4,38 @@ from statistics import Statistics
 
 
 def find_excel():
-    files, xslx_files = os.listdir(), []
-    for file in files:
-        if os.path.isfile(file):
-            if os.path.splitext(file)[1] == '.xlsx' and not file.startswith(r'~$'):
-                xslx_files.append(file)
-    return xslx_files
+    things, dirs, xslx_files = os.listdir(), [], []
+    for sth in things:
+        if os.path.isfile(sth):
+            if os.path.splitext(sth)[1] == '.xlsx' and not sth.startswith(r'~$'):
+                xslx_files.append(sth)
+                print('ADDED TO THE LIST: {} '.format(sth))
+        else:
+            dirs.append(sth)
+    return dirs, xslx_files
+
+
+def exclude_done(dirs, xlsx_files):
+    results = []
+    for xlsx_file in xlsx_files:
+        file_name = os.path.splitext(xlsx_file)[0]
+        if file_name not in dirs:
+            results.append(xlsx_file)
+        else:
+            dirs.remove(file_name)
+            print('REMOVED FROM THE LIST: {}'.format(xlsx_file))
+    return results
 
 
 def main():
     current_path = os.path.dirname(os.path.abspath(__file__))
     os.chdir(current_path)
-    xlsx_files = find_excel()
+    dirs, xlsx_files = find_excel()
+    xlsx_files = exclude_done(dirs, xlsx_files)
 
     if len(xlsx_files) == 0:
-        print('XLSX FILE NOT FOUND')
+        print('XLSX FILE NOT FOUND OR ALREADY DONE')
         exit()
-    elif len(xlsx_files) == 1:
-        statistics = Statistics()
-        statistics.count(xlsx_files[0])
     else:
         # length = len(xlsx_files)
         # threads = [None] * length
@@ -35,6 +48,7 @@ def main():
             statistics = Statistics()
             os.chdir(current_path)
             statistics.count(xlsx_file)
+            print('STATISTICS DONE: {}'.format(os.path.splitext(xlsx_file)[0]))
 
 
 if __name__ == '__main__':
